b9bebcac3f72d8ffb7090f0785e0cd7ca1944aff
shot83428 <shot83428@gmail.com>
2023-01-10 22:04:58 +0800

    20231/10

---
+++ Finish/100.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        
+		if(p==null&&q==null)
+			return true;
+		else if(p==null||q==null)
+			return false;
+		else if(p.val!=q.val)
+			return false;
+		
+		return IsSameTree(p.left,q.left)&&IsSameTree(p.right,q.right);
+    }
+}
--- Finish/84.cs
+++ Finish/84.cs
@@ -1,4 +1,4 @@
-using System.Collections.Concurrent;
+using System.Collections.Generic;
 public class Solution
 {
     public int LargestRectangleArea(int[] heights)
@@ -6,37 +6,23 @@ public class Solution
         int max = 0;
         Stack<int> stacks = new Stack<int>();
         Stack<int> stacksCount = new Stack<int>();
-        int count = 0;
-        stacks.Push(heights[0]);
-        stacksCount.Push(1);
-        for (int index = 1; index < heights.Length; index++)
+        int count = 1;
+        for (int index = 0; index < heights.Length; count = 1, index++)
         {
-            if (stacks.Peek() <= heights[index])
+            if (stacks.TryPeek(out var peek) && peek < heights[index])
             {
                 stacks.Push(heights[index]);
-                stacksCount.Push(1);
+                stacksCount.Push(count);
             }
             else
             {
-                count = 0;
-                while (stacks.Count() > 0 && stacks.Peek() > heights[index])
+                while (stacks.TryPeek(out peek) && peek >= heights[index])
                 {
                     count += stacksCount.Pop();
-                    max = Math.Max(max, count * stacks.Pop());
+                    max = Math.Max(max, (count - 1) * stacks.Pop());
                 }
-                if (stacks.Count() > 0)
-                {
-                    var tmp = stacksCount.Pop();
-                    tmp += count;
-                    stacksCount.Push(tmp);
-                    stacks.Push(heights[index]);
-                    stacksCount.Push(1);
-                }
-                else
-                {
-                    stacksCount.Push(count + 1);
-                    stacks.Push(heights[index]);
-                }
+                stacks.Push(heights[index]);
+                stacksCount.Push(count);
             }
         }
         count = 0;
@@ -51,33 +37,34 @@ public class Solution
 }
 
 //no good
-public class Solution {
+public class Solution
+{
     public int LargestRectangleArea(int[] heights)
+    {
+        int max = 0;
+        int len = heights.Length;
+        int[] dp = new int[len];
+        for (int index = 0; index < heights.Length; index++)
+        {
+            if (index > 0 && heights[index] == heights[index - 1])
+                continue;
+            dp[index] += heights[index];
+            int count = heights[index] > 0 ? dp[index] / heights[index] : 0;
+            for (int af = index + 1; af < heights.Length; af++)
             {
-                int max = 0;
-                int len = heights.Length;
-                int[] dp = new int[len];
-                for (int index = 0; index < heights.Length; index++)
+                if (heights[index] > heights[af])
                 {
-                    if (index > 0 && heights[index] == heights[index - 1])
-                        continue;
-                    dp[index] += heights[index];
-                    int count = heights[index] > 0 ? dp[index] / heights[index] : 0;
-                    for (int af = index + 1; af < heights.Length; af++)
-                    {
-                        if (heights[index] > heights[af])
-                        {
-                            dp[af] = Math.Max((count + af - index - 1) * heights[af], dp[af]);
-                            max = Math.Max(max, dp[af]);
-                            break;
-                        }
-                        else
-                        {
-                            dp[index] += heights[index];
-                        }
-                    }
-                    max = Math.Max(max, dp[index]);
+                    dp[af] = Math.Max((count + af - index - 1) * heights[af], dp[af]);
+                    max = Math.Max(max, dp[af]);
+                    break;
                 }
-                return max;
+                else
+                {
+                    dp[index] += heights[index];
+                }
             }
+            max = Math.Max(max, dp[index]);
+        }
+        return max;
+    }
 }
