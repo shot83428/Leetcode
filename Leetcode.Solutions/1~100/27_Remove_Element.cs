1c22c443293b368b23b92daa4ee340c06b4e64ca
DESKTOP-L9551PK\User <shot83428@gmail.com>
2023-05-13 14:41:56 +0800

    20230513

---
+++ Leetcode.Solutions/1~100/27_Remove_Element.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Solutions
+{
+    public class Solution_27
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int index = nums.Length - 1;
+            for (int j = 0; j < index; j++)
+            {
+                if (nums[j] == val)
+                {
+                    (nums[j], nums[index]) = (nums[index], nums[j]);
+                    j--;
+                    index--;
+                }
+            }
+            if (nums.Length != 0 && nums[index] == val)
+            {
+                return index;
+            }
+            return index + 1;
+        }
+    }
+}
---
+++ Leetcode.Solutions/2401~2500/2466_Count_Ways_To_Build_Good_Strings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Solutions
+{
+    public class Solution_2466
+    {
+        public int CountGoodStrings(int low, int high, int zero, int one)
+        {
+            int[] dp = new int[high];
+
+            dp[zero - 1]++;
+            dp[one - 1]++;
+
+            for (int index = 0; index < high; index++)
+            {
+                if (index - zero >= 0)
+                {
+                    dp[index] += dp[index - zero];
+                }
+                if (index - one >= 0)
+                {
+                    dp[index] += dp[index - one];
+                }
+                dp[index] %= 1000000007;
+            }
+
+            var result = 0;
+            for (int index = low - 1; index < high; index++)
+            {
+                result += dp[index];
+                result %= 1000000007;
+            }
+
+            return result;
+        }
+    }
+}
