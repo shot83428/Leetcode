940f9fc5504466aef67daf9379a47f6a662bf698
DESKTOP-L9551PK\User <shot83428@gmail.com>
2023-05-21 23:29:31 +0800

    20230521

---
+++ Leetcode.Solutions/0301~0400/399_Evaluate_Division.cs
@@ -0,0 +1,91 @@
+﻿namespace Leetcode.Solutions
+{
+    public class Solution_399
+    {
+        private IList<bool> reached = new List<bool>();
+
+        private IList<IList<Tuple<int, double>>> adj;
+
+        public double[] CalcEquation(IList<IList<string>> equations, double[] values, IList<IList<string>> queries)
+        {
+            IDictionary<string, int> strToInt = new Dictionary<string, int>();
+            adj = new List<IList<Tuple<int, double>>>();
+            int cnt = 0;
+
+
+            foreach (IList<string> curr in equations)
+            {
+                if (strToInt.ContainsKey(curr[0]) == false)
+                {
+                    strToInt.Add(curr[0], cnt++);
+                    adj.Add(new List<Tuple<int, double>>());
+                    reached.Add(false);
+                }
+
+                if (strToInt.ContainsKey(curr[1]) == false)
+                {
+                    strToInt.Add(curr[1], cnt++);
+                    adj.Add(new List<Tuple<int, double>>());
+                    reached.Add(false);
+                }
+            }
+
+            for (int index = 0; index < equations.Count; ++index)
+            {
+                IList<string> curr = equations[index];
+
+                var edge = new Tuple<int, double>(strToInt[curr[1]], values[index]);
+                var edge2 = new Tuple<int, double>(strToInt[curr[0]], 1 / values[index]);
+
+                adj[strToInt[curr[0]]].Add(edge);
+                adj[strToInt[curr[1]]].Add(edge2);
+            }
+
+
+            double[] res = new double[queries.Count];
+            for (int index = 0; index < queries.Count; ++index)
+            {
+                IList<string> query = queries[index];
+                if (strToInt.ContainsKey(query[0]) == false || strToInt.ContainsKey(query[1]) == false)
+                {
+                    res[index] = -1.0;
+                }
+                else
+                {
+                    int node1 = strToInt[query[0]];
+                    int node2 = strToInt[query[1]];
+
+                    res[index] = dfs(node1, node2, 1);
+                    for (int i = 0; i < reached.Count; ++i)
+                    {
+                        reached[i] = false;
+                    }
+                }
+            }
+
+
+            return res;
+        }
+
+        private double dfs(int currNode, int targetNode, double currVal)
+        {
+            if (currNode == targetNode)
+            {
+                return currVal;
+            }
+
+            reached[currNode] = true;
+
+            double max = -1;
+            foreach (Tuple<int, double> curr in adj[currNode])
+            {
+                if (reached[curr.Item1] == false)
+                {
+                    max = Math.Max(dfs(curr.Item1, targetNode, currVal * curr.Item2), max);
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ Leetcode.Solutions/0901~1000/934_Shortest_Bridge.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Solutions
+{
+    public class Solution_934
+    {
+        private int[][] path = new int[4][] {
+        new int[] { 1, 0 }
+        , new int[] { -1, 0 }
+        , new int[] { 0,1 }
+        , new int[] { 0, -1 } };
+        public int ShortestBridge(int[][] grid)
+        {
+            (int x, int y) = FindIsland(grid);
+            var bfs = GetWatch(grid, x, y);
+            int n = grid.Length;
+
+            while (bfs.TryDequeue(out var item))
+            {
+                foreach (var way in path)
+                {
+                    if (!IsInGrid(item.Item1 + way[0], item.Item2 + way[1], n))
+                    {
+                        continue;
+                    }
+                    if (grid[item.Item1 + way[0]][item.Item2 + way[1]] == 0)
+                    {
+                        grid[item.Item1 + way[0]][item.Item2 + way[1]] = grid[item.Item1][item.Item2] - 1;
+                        bfs.Enqueue((item.Item1 + way[0], item.Item2 + way[1]));
+                    }
+                    else if (grid[item.Item1 + way[0]][item.Item2 + way[1]] == 1)
+                    {
+                        return grid[item.Item1][item.Item2] * -1;
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private (int, int) FindIsland(int[][] grid)
+        {
+            int x;
+            int y;
+            int n = grid.Length;
+            for (x = 0; x < grid.Length; x++)
+            {
+                for (y = 0; y < grid[x].Length; y++)
+                {
+
+                    if (!IsInGrid(x, y, n))
+                    {
+                        continue;
+                    }
+                    if (grid[x][y] != 0)
+                    {
+                        return (x, y);
+                    }
+                }
+            }
+            return (0, 0);
+        }
+
+        private Queue<(int, int)> GetWatch(int[][] grid, int x, int y)
+        {
+            int n = grid.Length;
+            Queue<(int, int)> queue = new();
+            Queue<(int, int)> queue2 = new();
+            queue2.Enqueue((x, y));
+            grid[x][y] = 2;
+            while (queue2.TryDequeue(out var item))
+            {
+                foreach (var way in path)
+                {
+                    if (!IsInGrid(item.Item1 + way[0], item.Item2 + way[1], n))
+                    {
+                        continue;
+                    }
+                    if (grid[item.Item1 + way[0]][item.Item2 + way[1]] == 1)
+                    {
+                        grid[item.Item1 + way[0]][item.Item2 + way[1]] = 2;
+                        queue2.Enqueue((item.Item1 + way[0], item.Item2 + way[1]));
+                    }
+                    else if (grid[item.Item1 + way[0]][item.Item2 + way[1]] == 0)
+                    {
+                        grid[item.Item1 + way[0]][item.Item2 + way[1]] = -1;
+                        queue.Enqueue((item.Item1 + way[0], item.Item2 + way[1]));
+                    }
+                }
+            }
+
+            return queue;
+        }
+        private bool IsInGrid(int x, int y, int n)
+        {
+            if (x < 0 || y < 0) return false;
+            if (x >= n || y >= n) return false;
+            return true;
+        }
+    }
+}
